50635b0822cd1acbf89274e96a098814b0d3bba2
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-02-06 09:33:28 +0100

    first version

---
+++ js/app.js
@@ -0,0 +1,75 @@
+angular.module("quickDemoMovies",['ngRoute'])
+  .factory("MoviesFactory", function($http) {
+
+    var apiKey = 'ba09f3c8c6c830377b422df18cfa833e'
+
+    function getPopular(){
+      var url = 'https://api.themoviedb.org/3/movie/popular?api_key=<%KEY%>'
+      var url = url.replace('<%KEY%>',apiKey)
+      return $http.get(url)
+    }
+
+    function getUpcoming(){
+      var url = 'https://api.themoviedb.org/3/movie/upcoming?api_key=<%KEY%>'
+      var url = url.replace('<%KEY%>',apiKey)
+      return $http.get(url)
+    }
+
+    function getNowPlaying(){
+      var url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=<%KEY%>'
+      var url = url.replace('<%KEY%>',apiKey)
+      return $http.get(url)
+    }
+
+    function getTopRated(){
+      var url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=<%KEY%>'
+      var url = url.replace('<%KEY%>',apiKey)
+      return $http.get(url)
+    }
+
+    return {
+      getPopular: getPopular,
+      getUpcoming: getUpcoming,
+      getNowPlaying: getNowPlaying,
+      getTopRated: getTopRated
+    }
+
+  })
+  .controller('UpcomingController', function($scope,MoviesFactory) {
+
+    MoviesFactory.getUpcoming()
+      .then( function(response) {
+        $scope.movies = response.data.results
+      })
+
+  })
+  .controller('PopularController', function($scope,MoviesFactory) {
+
+    MoviesFactory.getPopular()
+      .then( function(response) {
+        $scope.movies = response.data.results
+      })
+
+  })
+    .controller('PopularController', function($scope,MoviesFactory) {
+
+    MoviesFactory.getPopular()
+      .then( function(response) {
+        $scope.movies = response.data.results
+      })
+
+  })
+  .config(function($routeProvider) {
+
+    $routeProvider
+      .when('/upcoming',{
+        templateUrl: 'templates/main.html',
+        controller: 'UpcomingController'
+      })
+      .when('/popular',{
+        templateUrl: 'templates/main.html',
+        controller: 'PopularController'
+      })
+      .otherwise('/popular')
+
+  })
